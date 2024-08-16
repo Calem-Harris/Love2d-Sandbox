a642a78b1da143508182fe7a63765cc0f2b35b6a
Calem Harris <melacsirrah575@gmail.com>
2024-08-15 22:41:13 -0300

    Day 4 Code Along
    Talk on if statements, conditions, and made some shapes move around on my screen

--- main.lua
+++ main.lua
@@ -12,26 +12,71 @@ function love.errorhandler(msg)
     end
 end
 
-print(type("Hello World")); -- String --
-print(type(10.4)); -- Number --
-print(type(print)); -- function --
-print(type(true)); -- Boolean --
-print(type(nil)); -- Nil -- 
+-- print(type("Hello World")); -- String --
+-- print(type(10.4)); -- Number --
+-- print(type(print)); -- function --
+-- print(type(true)); -- Boolean --
+-- print(type(nil)); -- Nil -- 
 
-thing1 = function()
-    print("Hello World")
+-- thing1 = function()
+--     print("Hello World")
+-- end
+
+-- thing1()
+-- thing1()
+
+-- function example()
+--     print("Hello World Again!")
+-- end
+
+-- example()
+-- example()
+
+function love.load()
+    rectangleXPos = 100
+    rectangleYPos = 50
+    circleXPos = 300
+    circleYPos = 300
+    rectangleMoveSpeed = 100
+    circleMoveSpeed = 200
 end
 
-thing1()
-thing1()
+function love.update(deltaTime)
+    --print(deltaTime)
 
-function example()
-    print("Hello World Again!")
+    -- > Greater Than
+    -- < Less Than
+    -- == Is Equal To
+    -- <= Less Than or Equal To
+    -- >= Greater Than or Equal To
+    -- ~= Is NOT Equal To
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+        rectangleXPos = rectangleXPos + rectangleMoveSpeed * deltaTime
+    elseif love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+        rectangleXPos = rectangleXPos - rectangleMoveSpeed * deltaTime
+    end
+
+    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+        rectangleYPos = rectangleYPos - rectangleMoveSpeed * deltaTime
+    elseif love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+        rectangleYPos = rectangleYPos + rectangleMoveSpeed * deltaTime
+    end
+
+    -- if love.keyboard.isDown("a") then
+    --     circleXPos = circleXPos - circleMoveSpeed * deltaTime
+    -- elseif love.keyboard.isDown("d") then
+    --     circleXPos = circleXPos + circleMoveSpeed * deltaTime
+    -- end
+
+    -- if love.keyboard.isDown("w") then
+    --     circleYPos = circleYPos - circleMoveSpeed * deltaTime
+    -- elseif love.keyboard.isDown("s") then
+    --     circleYPos = circleYPos + circleMoveSpeed * deltaTime
+    -- end
+
 end
-
-example()
-example()
 
 function love.draw()
-    love.graphics.rectangle("line", 100, 200, 50, 80)
+    love.graphics.rectangle("line", rectangleXPos, rectangleYPos, 200, 150)
+    --love.graphics.circle("line", circleXPos, circleYPos, 50)
 end
